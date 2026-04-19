9b1ac02cc697fe9a58a72356e149be1d915e37fe
Ahmed Aziz Amri <ahmedazizamri@hotmail.com>
2026-04-19 19:58:25 +0100

    done

---
+++ challenge/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const Cards = [
+  {
+    id: 3457,
+    question: "What language is React Based on ?",
+    answer: "javascript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of react apps ?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question:
+      "What's the name of the syntaxe we use to describe a ui in React ?",
+    answer: "JSX",
+  },
+
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components ?",
+    answer: "Props",
+  },
+
+  {
+    id: 9103,
+    question: "How to give components memory ?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completly synchronised with state ?",
+    answer: "Controlled element",
+  },
+];
+export default function FlashCards() {
+  return (
+    <div className="cards">
+      {Cards.map((ele) => (
+        <Card obj={ele} />
+      ))}
+    </div>
+  );
+}
+
+function Card({ obj }) {
+  const [content, setContent] = useState(obj.question);
+  function showAnswer() {
+    setContent(obj.answer);
+  }
+  function showQuestion() {
+    setContent(obj.question);
+  }
+  return (
+    <div
+      className={content === obj.question ? "question" : "answer"}
+      onClick={showAnswer}
+      onMouseLeave={showQuestion}
+    >
+      {content}
+    </div>
+  );
+}
---
+++ challenge/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import FlashCards from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <FlashCards />
+  </React.StrictMode>,
+);
